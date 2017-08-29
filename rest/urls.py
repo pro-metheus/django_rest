0aebe3027417e20717d642fc2ea5053f0532d0dc
pro-metheus <ajayajith19@gmail.com>
2017-08-29 07:44:54 +0530

    Add files via upload
    added correct url pattern regex

--- rest/urls.py
+++ rest/urls.py
@@ -3,5 +3,5 @@ from .views import api_handler
 
 urlpatterns=[
 
-url(r'^api/(?P<api_key>[0-9a-f]{32}\Z)/(?P<function>[a-z]+)',api_handler)
+url(r'^api/(?P<api_key>[0-9a-f]{32})/(?P<function>[a-z])/', api_handler)
 ]
